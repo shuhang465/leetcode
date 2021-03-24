f583c8139b1321a25fb0f4a930ede1856cfb3261
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-24 19:24:32 +0800

    Create 167.two_sum_sorted.py

---
+++ double_pointer/167.two_sum_sorted.py
@@ -0,0 +1,11 @@
+#和之前用字典做法相比，这个数组是有序的，所以和木桶接水一样用双指针，比target大就往小的去，反之亦然
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left ,right = 0, len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right]  == target:
+                return [left + 1, right + 1]
+            if numbers[left] + numbers[right]  < target:
+                left += 1
+            if numbers[left] + numbers[right]  > target: 
+                right -= 1
