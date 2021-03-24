aa059a5c91bab69c42f99560df0c2e0e2308f733
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-24 22:28:02 +0800

    Create 35.search_insert_position.py

---
+++ binary_search/35.search_insert_position.py
@@ -0,0 +1,12 @@
+#二分查找返回值为[first,last)内第一个不小于value的值的位置
+#给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left,right = 0, len(nums)
+        while left<right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left
