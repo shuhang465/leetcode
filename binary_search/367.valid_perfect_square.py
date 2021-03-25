977a9149b934577225955ce82dea794d392b3c94
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-25 21:10:22 +0800

    Create 367.valid_perfect_square.py

---
+++ binary_search/367.valid_perfect_square.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 0
+        right = num
+        if num == 1:
+            return True
+        while left < right:
+            mid = left + (right-left)//2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid
+        return False
