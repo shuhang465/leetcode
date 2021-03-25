55411604afc5f6252664f77a7e5041f6459a4ba9
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-25 22:42:16 +0800

    Update 441.arranging_coins.py

--- binary_search/441.arranging_coins.py
+++ binary_search/441.arranging_coins.py
@@ -3,14 +3,14 @@ class Solution:
     def arrangeCoins(self, n: int) -> int:
         left = 1
         right = n
-        while left < right:
+        while left <= right:
             mid = left + (right-left)//2
             if (mid+1)*mid//2 == n:
                 return mid
             if (mid+1)*mid//2 < n:
                 left = mid + 1
             else:
-                right = mid
+                right = mid -1
         if (left+1)*left//2 > n:
             return left - 1
         else:
