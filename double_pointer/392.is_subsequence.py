2f5a5c20f6060562c23e695a665de33ecc9359f5
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-25 22:08:36 +0800

    Create 392.is_subsequence.py

---
+++ double_pointer/392.is_subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        n, m = len(s), len(t)
+        i = j = 0
+        while i < n and j < m:
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        #这个临界点还挺有意思的
+        return i == n
