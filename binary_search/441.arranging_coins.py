23947303125ee9c3ab0dbf55fb05e2196c73f4e1
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-25 22:23:20 +0800

    Create 441.arranging_coins.py

---
+++ binary_search/441.arranging_coins.py
@@ -0,0 +1,17 @@
+#和69题很像
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left = 1
+        right = n
+        while left < right:
+            mid = left + (right-left)//2
+            if (mid+1)*mid//2 == n:
+                return mid
+            if (mid+1)*mid//2 < n:
+                left = mid + 1
+            else:
+                right = mid
+        if (left+1)*left//2 > n:
+            return left - 1
+        else:
+            return left
