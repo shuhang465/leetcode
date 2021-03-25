3140deb6aa337859b202d3e1069007c681b7d8e1
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-25 16:01:00 +0800

    Update 345.reverse_vowel_of_a_string.py

--- double_pointer/345.reverse_vowel_of_a_string.py
+++ double_pointer/345.reverse_vowel_of_a_string.py
@@ -4,16 +4,16 @@ class Solution:
         lst = list(s)
         n = len(s)
         l, r = 0, n - 1
-
         while l < r:
-            if lst[l] in dic and lst[r] in dic:
+            #这里要再加一个l<r的判断是因为，到最后剩下两个元素的时候，“，。”这样left加一，right减一，leftright的顺序就颠倒了，然后再次执行l+1,r-1就会out of range
+            while l < r and lst[l] not in dic:
+                l += 1
+            while l < r and  lst[r] not in dic:
+                r -= 1
+            if l < r:
                 lst[l], lst[r] = lst[r], lst[l]
-                l = l + 1
-                r = r - 1
-            elif lst[l] not in dic:
-                l = l + 1
-            elif lst[r] not in dic:
-                r = r - 1
+            l = l + 1
+            r = r - 1
+          
     
         return ''.join(lst)
-
