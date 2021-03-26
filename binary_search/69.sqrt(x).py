987cbb4eeb1e670aff0d6d56da9f31a2cf29a845
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-03-26 16:41:54 +0800

    Update 69.sqrt(x).py

--- binary_search/69.sqrt(x).py
+++ binary_search/69.sqrt(x).py
@@ -3,12 +3,11 @@ class Solution:
         left, right = 0, x
         while left <= right:
             mid = left + (right-left)//2
+            if mid * mid == x:
+                return mid
             if mid * mid < x:
                 left = mid + 1
             else:
                 right = mid -1
-        if left * left > x:
-            return left-1
-        else:
-            return left
+        return right
         
