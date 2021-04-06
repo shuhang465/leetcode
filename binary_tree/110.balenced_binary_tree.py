2095dc769479baefdf6c6f39f58f36efcf4b1375
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-06 22:31:36 +0800

    Update 110.balenced_binary_tree.py

--- binary_tree/110.balenced_binary_tree.py
+++ binary_tree/110.balenced_binary_tree.py
@@ -9,7 +9,9 @@ class Solution:
         def height(root: TreeNode) -> int:
             if not root:
                 return 0
-            return max(height(root.left), height(root.right)) + 1
+            L = height(root.left)
+            R = height(root.right)
+            return max(L, R) + 1
         if not root:
             return True
         return abs(height(root.left) - height(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
