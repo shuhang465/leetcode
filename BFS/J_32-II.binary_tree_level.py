f5c1bbc63eb0f719db3614b90b2ed8fc6e8b2429
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-07 14:29:54 +0800

    Create J_32-II.binary_tree_level.py

---
+++ BFS/J_32-II.binary_tree_level.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        from collections import deque
+        queue = deque()
+        queue.append(root)
+        while queue:
+            #按层遍历
+            size = len(queue)
+            tmp = []
+            for _ in range(size):
+                node = queue.popleft()
+                tmp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(tmp)
+        return res
