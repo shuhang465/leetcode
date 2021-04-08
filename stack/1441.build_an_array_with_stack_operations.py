82a37a6bc333ebef87136097aab05f1be33a8d28
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-08 15:49:17 +0800

    Update 1441.build_an_array_with_stack_operations.py

--- stack/1441.build_an_array_with_stack_operations.py
+++ stack/1441.build_an_array_with_stack_operations.py
@@ -1,1 +1,12 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        res = []
+        for num in list(range(1,max(target)+1)):
+            if num not in target:
+                res.extend(["Push","Pop"])
+            else:
+                res.append("Push")
+        
+        return res
 
+#注意extend和append
