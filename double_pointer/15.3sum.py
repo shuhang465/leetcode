0b41435a1ce48fbe7737548aec9430d35c27f048
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-08 22:17:29 +0800

    Update 15.3sum.py

--- double_pointer/15.3sum.py
+++ double_pointer/15.3sum.py
@@ -1,1 +1,40 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        if len(nums) < 3:
+            return ans
+        nums.sort()
+        for i in range(len(nums)-2):
+            # 如果最小的三个数已经大于 0，退出程序
+            if nums[i] + nums[i+1] + nums[i+2] > 0:
+                break
+            # 如果最大的三个数还小于0，continue
+            if nums[i] + nums[-1] + nums[-2] < 0:
+                continue
+            # 如果当前这个数等于前一个数， continue
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            # 双指针
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right]
+                # 如果三数之和等于 0，
+                if three_sum == 0:
+                    ans.append([nums[i], nums[left], nums[right]])
+                    # 去除重复的左边元素
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    # 去除重复的右边元素
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    # 更新 left， right的值
+                    left += 1
+                    right -= 1
+                # 如果三数之和小于 0
+                elif three_sum < 0:
+                    left += 1
+                # 如果三数之和大于 0
+                else:
+                    right -= 1
+        return ans
 
