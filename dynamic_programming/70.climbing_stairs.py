39c89d7ff784fc0204662d2876f59d116e44d035
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-13 00:23:25 +0800

    Update 70.climbing_stairs.py

--- dynamic_programming/70.climbing_stairs.py
+++ dynamic_programming/70.climbing_stairs.py
@@ -1,8 +1,10 @@
 class Solution:
-    # 一维dp，自底向上
+    # f(n)只依赖于f(n-1)和f(n-2)，只需要两项就足够了
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        dp[0] = dp[1] = 1
+        a = b = 1
         for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[-1]
+            a, b = b, a + b
+        return b
+
+
+
