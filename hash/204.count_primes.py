810ba572d0bea9712a928aa5acacf44901752561
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-12 23:21:33 +0800

    Update 204.count_primes.py

--- hash/204.count_primes.py
+++ hash/204.count_primes.py
@@ -1,12 +1,15 @@
 class Solution:
-    def countPrimes(n):
-        is_prime = [True]*(n+1)
-        ans = 0
-        for num in range(2,n+1):
-            if is_prime[num]:
-                ans+=1
-                for k in range(1,n//num+1):
-                    is_prime[num*k]=False
-        return ans
+    def countPrimes(self, n: int) -> int:
+        isprime = [True] * n
+        rt = 0
+        for i in range(2, n):
+            if isprime[i]:
+                rt += 1
+                j = i+i
+                while j < n:
+                    isprime[j]=False
+                    j += i
+        return rt
+
 #合数可以由若干质数相乘得到，所以质数的倍数是合数，假设所有数都是质数，然后除去倍数
         
