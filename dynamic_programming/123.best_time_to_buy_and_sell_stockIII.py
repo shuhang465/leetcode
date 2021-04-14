850a8bf1cb4ebaac5d45738f89361fc1c42c67f5
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-14 22:35:10 +0800

    Update 123.best_time_to_buy_and_sell_stockIII.py

--- dynamic_programming/123.best_time_to_buy_and_sell_stockIII.py
+++ dynamic_programming/123.best_time_to_buy_and_sell_stockIII.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        if n==0:
-            return 0
-        buy_1 = buy_2 = float('inf')
-        pro_1 = pro_2 = 0
-        for p in prices:
-            buy_1 = min(buy_1, p)
-            pro_1 = max(pro_1, p - buy_1)
-            buy_2 = min(buy_2, p - pro_1) # p - pro_1 是用第一次的钱抵消了一部分第二次买的钱
-            pro_2 = max(pro_2, p - buy_2)
-        return pro_2
-
+        buy1 = buy2 = -prices[0]
+        sell1 = sell2 = 0
+        for i in range(1, n):
+            #可以保持之前的买入不动或者买入
+            buy1 = max(buy1, -prices[i])
+            #可以保持之前的卖出不变或者卖出
+            sell1 = max(sell1, buy1 + prices[i])
+            buy2 = max(buy2, sell1 - prices[i])
+            sell2 = max(sell2, buy2 + prices[i])
+        return sell2
 
