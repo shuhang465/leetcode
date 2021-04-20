04b8f9b03de5a48327269a43108863408ef99ef2
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-20 16:10:30 +0800

    Update 703.kth_largest_element_in_a_stream.py

--- heap/703.kth_largest_element_in_a_stream.py
+++ heap/703.kth_largest_element_in_a_stream.py
@@ -1,1 +1,14 @@
+class KthLargest(object):
 
+    def __init__(self, k, nums):
+      
+        self.k = k
+        self.que = nums
+        heapq.heapify(self.que)
+
+    def add(self, val):
+        
+        heapq.heappush(self.que, val)
+        while len(self.que) > self.k:
+            heapq.heappop(self.que)
+        return self.que[0]
