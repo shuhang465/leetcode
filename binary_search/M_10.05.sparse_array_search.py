a6d15598eaa18c03b16db9768990b89950f59bbc
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-21 23:45:42 +0800

    Update M_10.05.sparse_array_search.py

--- binary_search/M_10.05.sparse_array_search.py
+++ binary_search/M_10.05.sparse_array_search.py
@@ -1,13 +1,17 @@
 class Solution:
     def findString(self, words: List[str], s: str) -> int:
+        #这道题注意是排好序的，所以可以用左右指针
         left,right=0,len(words)-1
         while left<=right:
+            #首先pass掉空格
             while words[left]=='':
                 left+=1
             while words[right]=='':
                 right-=1
+            #但是pass掉之后要判断是不是left还是小于right
             if left>right:
                 break
+            #然后再用二分查找
             mid=left+((right-left)//2)
             while words[mid]=='':
                 mid+=1
