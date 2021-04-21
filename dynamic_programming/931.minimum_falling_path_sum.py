444cc67326b9c6345345532d9266ec51483299ed
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-21 19:06:19 +0800

    Create 931.minimum_falling_path_sum.py

---
+++ dynamic_programming/931.minimum_falling_path_sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minFallingPathSum(self, A):
+        while len(A) >= 2:
+            row = A.pop()            
+            for i in range(len(row)):
+                A[-1][i] += min(row[max(0,i-1): min(len(row), i+2)])
+        return min(A[0])
+
+#倒着来
