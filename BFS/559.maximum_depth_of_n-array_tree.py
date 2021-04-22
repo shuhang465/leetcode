bd44b75afe0bb07c1af2fb879b25d29331ce6ec4
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-22 20:40:12 +0800

    Update 559.maximum_depth_of_n-array_tree.py

--- BFS/559.maximum_depth_of_n-array_tree.py
+++ BFS/559.maximum_depth_of_n-array_tree.py
@@ -5,6 +5,9 @@ class Node:
         self.val = val
         self.children = children
 """
+# 给定一个 N 叉树，找到其最大深度。
+
+# 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
 
 class Solution(object):
     def maxDepth(self, root):
