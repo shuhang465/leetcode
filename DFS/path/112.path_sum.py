1eaf3472390142961c14f34faad6566512536e14
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-22 22:24:17 +0800

    Update 112.path_sum.py

--- DFS/path/112.path_sum.py
+++ DFS/path/112.path_sum.py
@@ -1,1 +1,23 @@
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root: return False
+        #到叶子结点才判断
+        if not root.left and not root.right:
+            return sum == root.val
+        #sum一直在递归减少
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
