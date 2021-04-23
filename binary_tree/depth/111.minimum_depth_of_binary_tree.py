02c1449ac3612edd9750207348bddb8d67f8fab9
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-23 15:01:15 +0800

    Update 111.minimum_depth_of_binary_tree.py

--- binary_tree/depth/111.minimum_depth_of_binary_tree.py
+++ binary_tree/depth/111.minimum_depth_of_binary_tree.py
@@ -4,18 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+# 给定一个二叉树，找出其最小深度。
+# 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        self.ans = 1
-        def depth(root):
-            #注意这里返回的是0
+    def minDepth(self, root: TreeNode) -> int:
+        def height(root:TreeNode):
             if not root:
                 return 0
-            L = depth(root.left)
-            R = depth(root.right)
-            # 只比计算树的高度110多了这一句话，就是说在返回高度的同时更新ans,这里的直径不是必须经过根结点的，所以是对所有节点遍历
-            self.ans = max(self.ans, L + R + 1)
-            return max(L, R) + 1
-        depth(root)
-        return self.ans - 1
+            L = height(root.left)
+            R = height(root.right)
+            if not root.left or not root.right:
+                return max(L, R) + 1
+            return min(L, R) + 1
+        return height(root)
+
+
+
