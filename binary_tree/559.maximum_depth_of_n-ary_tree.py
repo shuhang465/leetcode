3ff3d14c985006b27d913e22bbfe94084f3d5354
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-23 14:01:42 +0800

    Create 559.maximum_depth_of_n-ary_tree.py

---
+++ binary_tree/559.maximum_depth_of_n-ary_tree.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+# 给定一个 N 叉树，找到其最大深度。
+
+# 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        elif root.children == []:
+            return 1
+        return max(self.maxDepth(c) for c in root.children)+1
+
+
