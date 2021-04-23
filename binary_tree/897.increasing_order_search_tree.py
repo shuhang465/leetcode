f2cf6d3b037c6cf689554dc2c9633d05263b22f0
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-04-23 16:40:29 +0800

    Update 897.increasing_order_search_tree.py

--- binary_tree/897.increasing_order_search_tree.py
+++ binary_tree/897.increasing_order_search_tree.py
@@ -4,16 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+# 给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
 class Solution:
     def increasingBST(self, root):
         list_in = []
+        res = []
         def inorder(root):
             if not root:
                 return []
-            return inorder(root.left) + [root.val] + inorder(root.right)
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+            return res
         res = inorder(root)
-
         ans = cur = TreeNode(None)
         for i in res: 
             cur.right = TreeNode(i)
