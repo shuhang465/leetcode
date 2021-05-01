4bafc4c0598be764b8c489317f6ce9e1e7e218bb
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-05-01 23:17:33 +0800

    Update 692.topk_frequent_words.py

--- heap/topK/692.topk_frequent_words.py
+++ heap/topK/692.topk_frequent_words.py
@@ -1,1 +1,17 @@
+class Solution:
+    def topKFrequent(self, words: List[int], k: int) -> List[int]:
+        count = collections.Counter(words)
+        heap = [(-val, key) for key, val in count.items()]
+        return [item[1] for item in heapq.nsmallest(k, heap)]
+ 
+#2
+# class Solution(object):
+#     def topKFrequent(self, words, k):
+#         count = collections.Counter(words)
+#         heap = [(-freq, word) for word, freq in count.items()]
+#         heapq.heapify(heap)
+#         return [heapq.heappop(heap)[1] for _ in range(k)]
 
+
+        
+    
