ff5ee009e03ed2b18927267aa4e5ae8202003450
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-05-02 15:28:16 +0800

    Create 42.trapping_rain_water.py

---
+++ double_pointer/wooden_barrel/42.trapping_rain_water.py
@@ -0,0 +1,20 @@
+# 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        left, right = 0, len(height) - 1
+        leftMax = rightMax = 0
+
+        while left < right:
+            leftMax = max(leftMax, height[left])
+            rightMax = max(rightMax, height[right])
+            if height[left] < height[right]:
+                ans += leftMax - height[left]
+                left += 1
+            else:
+                ans += rightMax - height[right]
+                right -= 1
+        
+        return ans
+
+#这道题也是双指针，也是左右木板哪边低往哪边走，只不过接水是左右分别设置一个max，用max-now累加分别计算左右接的水
