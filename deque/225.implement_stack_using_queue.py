faff23aa8abf54b0c373566c05f4eb9873cfa464
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-05-06 22:39:47 +0800

    Update and rename 252.implement_stack_using_queue.py to 225.implement_stack_using_queue.py

---
+++ deque/225.implement_stack_using_queue.py
@@ -0,0 +1,55 @@
+# 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通队列的全部四种操作（push、top、pop 和 empty）。
+
+# 实现 MyStack 类：
+
+# void push(int x) 将元素 x 压入栈顶。
+# int pop() 移除并返回栈顶元素。
+# int top() 返回栈顶元素。
+# boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+
+# 使用一个队列时，为了满足栈的特性，即最后入栈的元素最先出栈，同样需要满足队列前端的元素是最后入栈的元素。
+
+# 入栈操作时，首先获得入栈前的元素个数 nn，然后将元素入队到队列，再将队列中的前 nn 个元素（即除了新入栈的元素之外的全部元素）依次出队并入队到队列，此时队列的前端的元素即为新入栈的元素，且队列的前端和后端分别对应栈顶和栈底。
+
+# 由于每次入栈操作都确保队列的前端元素为栈顶元素，因此出栈操作和获得栈顶元素操作都可以简单实现。出栈操作只需要移除队列的前端元素并返回即可，获得栈顶元素操作只需要获得队列的前端元素并返回即可（不移除元素）。
+
+# 由于队列用于存储栈内的元素，判断栈是否为空时，只需要判断队列是否为空即可。
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = collections.deque()
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        n = len(self.queue)
+        self.queue.append(x)
+        for _ in range(n):
+            self.queue.append(self.queue.popleft())
+
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.queue.popleft()
+
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.queue[0]
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return not self.queue
+   
