c770d23c234bd7a85f984691e86606da5abcd76d
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-05-26 16:58:45 +0800

    Update 206.reverse_linked_list.py

--- linked_list/206.reverse_linked_list.py
+++ linked_list/206.reverse_linked_list.py
@@ -19,8 +19,6 @@ class Solution(object):
 		# 申请两个节点，pre和 cur，pre指向None
 		pre = None
 		cur = head
-		# 遍历链表，while循环里面的内容其实可以写成一行
-		# 这里只做演示，就不搞那么骚气的写法了
 		while cur:
 			# 记录当前节点的下一个节点
 			tmp = cur.next
