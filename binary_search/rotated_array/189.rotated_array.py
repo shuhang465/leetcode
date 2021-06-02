72aefcd0662e45079db9155bea6c58cac62348df
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-06-02 23:55:57 +0800

    Create 189.rotated_array.py

---
+++ binary_search/rotated_array/189.rotated_array.py
@@ -0,0 +1,27 @@
+
+# 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def reverse(left,right):
+            while left<right:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+                right-=1
+        n=len(nums)
+        # 向右移动的位置k可能会大于n，因此对n取余
+        k=k%n
+        if k==0 or n<2:
+            return 
+        # 以此为例：nums = [1,2,3,4,5,6,7], k = 3
+        # 先整个数组反转：[7,6,5,4,3,2,1]
+        reverse(0,n-1)
+        # 前k个反转：[5,6,7,4,3,2,1]
+        reverse(0,k-1)
+        # 后n-k个反转：[5,6,7,1,2,3,4]
+        reverse(k,n-1)
+    
+
