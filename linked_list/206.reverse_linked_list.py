4b28de5e9887be420f9a3275b55fe64cb920fedd
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-07-06 22:59:45 +0800

    Update 206.reverse_linked_list.py

--- linked_list/206.reverse_linked_list.py
+++ linked_list/206.reverse_linked_list.py
@@ -9,7 +9,7 @@
 # 第二个指针 cur 指向 head，然后不断遍历 cur。
 # 每次迭代到 cur，都将 cur 的 next 指向 pre，然后 pre 和 cur 前进一位。
 # 都迭代完了(cur 变成 null 了)，pre 就是最后一个节点了。
-
+#迭代，时间复杂度O(n),空间复杂度O(1)
 class Solution(object):
 	def reverseList(self, head):
 		"""
