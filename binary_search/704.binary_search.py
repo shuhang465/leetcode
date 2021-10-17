be18c6dcb21c9058c797acf2d62167cd7c70543c
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-10-17 23:28:07 +0800

    Update 704.binary_search.py

--- binary_search/704.binary_search.py
+++ binary_search/704.binary_search.py
@@ -1,8 +1,11 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        #全局变量
         left = 0
         right = len(nums)-1
+        
         while left<=right:
+            #mid是在里面变化的，局部变量
             mid = left + (right - left) // 2
             if nums[mid] == target:
                 return mid
