9b5703c3a9a0a647fe3302e048f8595ab2328801
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-10-17 23:34:10 +0800

    Update 278.first_bad_version.py

--- binary_search/278.first_bad_version.py
+++ binary_search/278.first_bad_version.py
@@ -3,6 +3,22 @@
 # @return an integer
 # def isBadVersion(version):
 
+#new,因为肯定有一个错误的版本，所以直接返回left比较简单
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 0
+        right = n
+        while left <= right:
+            mid = left + (right- left)//2
+            if isBadVersion(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left 
 
 class Solution:
     def firstBadVersion(self, n):
