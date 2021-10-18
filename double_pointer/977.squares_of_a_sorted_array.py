985a93361b00957701fcde2d5c1d42ed09d0a0e6
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-10-18 23:47:04 +0800

    Update 977.squares_of_a_sorted_array.py

--- double_pointer/977.squares_of_a_sorted_array.py
+++ double_pointer/977.squares_of_a_sorted_array.py
@@ -3,9 +3,9 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         #return sorted(num * num for num in nums)
         #因为是升序排列的，所以按照函数图像，平方最大值肯定处在函数的两侧
-        #故左右指针，左右往里靠
         n = len(nums)
         ans = [0] * n
+        #这里加了一个pos变量，不断的为pos位置赋值，就是不断的通过比较左右两端的大小
         left , right , pos = 0, n-1, n-1
         while left <= right:
             if nums[left] * nums[left] > nums[right] * nums[right]:
