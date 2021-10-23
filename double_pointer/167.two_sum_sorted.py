47ce27e11b57c860e7eba86f6795ccc0be36a327
shuhang465 <81236995+shuhang465@users.noreply.github.com>
2021-10-23 23:53:02 +0800

    Update 167.two_sum_sorted.py

--- double_pointer/167.two_sum_sorted.py
+++ double_pointer/167.two_sum_sorted.py
@@ -2,6 +2,7 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left ,right = 0, len(numbers) - 1
+        #两数之和，要有两个数，终止条件是左等于右这样就只有一个数了
         while left < right:
             if numbers[left] + numbers[right]  == target:
                 return [left + 1, right + 1]
